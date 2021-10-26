b7e8bea2cab60bf818bd0914e86cf1000859d01c
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-26 22:33:18 +0200

    Finally showing comics

--- xkcd/HomeView.swift
+++ xkcd/HomeView.swift
@@ -8,20 +8,35 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var comics = ComicsModel()
+    
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "xkcd", size: 34)!]
         UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "xkcd", size: 17)!]
     }
+    
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVStack(spacing: 30) {
-                    ForEach(0..<10, id: \.self) { item in
-                        ComicCardView()
+                LazyVStack(spacing: -20) {
+                    ForEach(comics, id:\.id) { item in
+                        ComicCardView(comic: item)
                     }
                 }
             }
             .navigationBarTitle("xkcd")
+            .onAppear(perform: getComics)
+        }
+    }
+    
+    func getComics() {
+        NetworkManager.shared.getComics(since: 2499) { result in
+            switch result {
+            case .success(let comics):
+                self.comics = comics.reversed()
+            case .failure(let error):
+                print(error)
+            }
         }
     }
 }
