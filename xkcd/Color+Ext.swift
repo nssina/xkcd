15bd7c7bee842d042685db6cc07f6e3603765a57
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-26 16:04:32 +0200

    Added Color extension

---
+++ xkcd/Color+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Ext.swift
+//  xkcd
+//
+//  Created by Sina Rabiei on 10/26/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let bgOrange = Color(UIColor.orange.withAlphaComponent(0.07))
+}
