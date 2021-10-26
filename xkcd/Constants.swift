b41b5fa6a8e1812503f34c7189a1e6592136ced2
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-26 14:57:01 +0200

    Created Constants file
    • Added SFSymbols enum

---
+++ xkcd/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  xkcd
+//
+//  Created by Sina Rabiei on 10/26/21.
+//
+
+import Foundation
+import SwiftUI
+
+enum SFSymbols {
+    static let comics = Image(systemName: "rectangle.3.group.bubble.left")
+    static let search = Image(systemName: "magnifyingglass")
+    static let favorite = Image(systemName: "star")
+}
