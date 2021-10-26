00a36c527450fb0a6c41bd106bf42bf8c7b279af
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-26 16:05:10 +0200

    Added send and arrow icon in SFSymbols enum

--- xkcd/Constants.swift
+++ xkcd/Constants.swift
@@ -12,4 +12,6 @@ enum SFSymbols {
     static let comics = Image(systemName: "rectangle.3.group.bubble.left")
     static let search = Image(systemName: "magnifyingglass")
     static let favorite = Image(systemName: "star")
+    static let send = Image(systemName: "paperplane")
+    static let arrow = Image(systemName: "arrow.forward")
 }
