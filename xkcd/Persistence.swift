fc3bbdbae54a161cefa45e7e6e55a2b29ca7adec
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-28 20:34:53 +0200

    Configured PersistenceController

--- xkcd/Persistence.swift
+++ xkcd/Persistence.swift
@@ -9,47 +9,54 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
+    let container: NSPersistentContainer
+    
     static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
+        let controller = PersistenceController(inMemory: true)
+        let viewContext = controller.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let comic = Favorite(context: controller.container.viewContext)
+            comic.title = "Sina"
+            comic.alt = "alt"
+            comic.id = Int16(1)
+            comic.image = Data()
+            comic.date = "Oct 28, 2021"
+            comic.explainURL = "https://shortcut.io" // =)
         }
+        
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
-        return result
+        
+        return controller
     }()
 
-    let container: NSPersistentContainer
-
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "xkcd")
+        container = NSPersistentContainer(name: "Favorite")
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+    }
+    
+    func saveComic() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Error occurs while saving comic: \(error.localizedDescription)")
+            }
+        }
     }
 }
